c0c9334c337bfe6eccfa45fd168b876e7eff9156
Abi_Abishek <plrabiyt@gmail.com>
2024-02-25 16:51:26 +0000

    Importing Projects

---
+++ Kovai_SF_Express/Kovai_SF_Express.ino
@@ -0,0 +1,12 @@
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(13,12,11,10,9,8);
+void setup() {
+  lcd.begin(16,2);
+}
+
+void loop() {
+ lcd.setCursor(0,0);
+ lcd.print("  Train No: 12676 - Kovai SF Express ");
+ lcd.scrollDisplayLeft();
+ delay(500);
+}
